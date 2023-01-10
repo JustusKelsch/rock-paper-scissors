359e7c50003d5985aa11bd806130f5aca50fa1a2
Justus Kelsch <33078472+JustusKelsch@users.noreply.github.com>
2023-01-09 22:02:29 -0700

    Added images to make the game more visually friendly. Added a reset button so the game can be restarted at any point. Centered the items horizontally. Added a bit of spacing to make the game a bit more visually appealing.

--- script.js
+++ script.js
@@ -4,14 +4,16 @@ const buttons = Array.from(document.getElementsByTagName("button"));
 const results = document.querySelector('.results');
 const scoreboard = document.querySelector('.score');
 
+function capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1);
+}
+
 function getComputerChoice() {
     const choice = ["Rock", "Paper", "Scissors"];
     return choice[Math.floor(Math.random() * 3)]
 }
 
 function playRound(playerSelection, computerSelection) {
-    
-    
     
     if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
         results.innerHTML = "It is a tie!"
@@ -22,18 +24,27 @@ function playRound(playerSelection, computerSelection) {
                 playerScore++;
                 results.innerHTML = "You Win! " + playerSelection + " beats " + computerSelection + "!"
     }
+    else if (playerSelection.toLowerCase() === 'reset') {
+        results.innerHTML = "Select a weapon to start the game!"
+        playerScore = 0;
+        computerScore = 0;
+    }
     else {
         computerScore++;
         results.innerHTML = "You Lose! " + computerSelection + " beats " + playerSelection + "!"
     }
 
     if (playerScore >= 5) {
-        results.innerHTML = "You Win!"
+        results.innerHTML = "You Win! Press \"Reset\" to play again."
         scoreboard.innerHTML = "Final Score: Player Score: " + playerScore + " Computer Score: " + computerScore;
+        playerScore = 0;
+        computerScore = 0;
     }
     else if (computerScore >= 5) {
-        results.innerHTML = "You Lose!"
+        results.innerHTML = "You Lose! Press \"Reset\" to play again."
         scoreboard.innerHTML = "Final Score: Player Score: " + playerScore + " Computer Score: " + computerScore;
+        playerScore = 0;
+        computerScore = 0;
     }
     else {
         scoreboard.innerHTML = "Player Score: " + playerScore + " Computer Score: " + computerScore;
@@ -43,9 +54,8 @@ function playRound(playerSelection, computerSelection) {
 function game() {
     
     buttons.forEach(button => {
-        button.addEventListener('click', function () { playRound(button.innerHTML, getComputerChoice())});
+        button.addEventListener('click', function () { playRound(capitalize(button.className), getComputerChoice())});
     })
-
 
 }
 
